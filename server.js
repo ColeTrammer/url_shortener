c6ffd951b068f235890d4acbcc809a0bcad6348e
ColeTrammer <cole.trammer@gmail.com>
2017-05-22 17:44:41 -0700

    Setup MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/url_shortener";
+
+MongoClient.connect(url, (err, db) => {
+    if (err) throw err;
+    console.log("Connected to database.");
+    db.close();
+});
+
 app.connect(process.env.PORT || 3000, () => {
-    console.log(`Connected to port ${process.env.PORT || 3000}`);
+    console.log(`Connected to port ${process.env.PORT || 3000}.`);
 });
