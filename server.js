206c2ab8baf99af376d1ea8bda3f7570ab803f85
ColeTrammer <cole.trammer@gmail.com>
2017-05-22 18:12:23 -0700

    set up mongodb for heroku

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require("express");
 const app = express();
 
 const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://localhost:27017/url_shortener";
 
-MongoClient.connect(url, (err, db) => {
+MongoClient.connect(process.env.MONGODB_URL, (err, db) => {
     if (err) throw err;
     console.log("Connected to database.");
     db.close();
 });
 
-app.connect(process.env.PORT || 3000, () => {
-    console.log(`Connected to port ${process.env.PORT || 3000}.`);
+app.connect(process.env.PORT, () => {
+    console.log(`Connected to port ${process.env.PORT}.`);
 });
